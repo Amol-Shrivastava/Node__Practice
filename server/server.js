e36886712f19c9db3720de3697b62996aa4e16a8
amol <amolshrivastava448@gmail.com>
2022-11-02 22:49:44 +0530

    Adding Static file depending upon request header

--- server/server.js
+++ server/server.js
@@ -1,57 +1,51 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const { send } = require('process');
 
-
-const send404 = (res) => {
-const msg = 'Error 404 => Resource cannot be found';
- res.writeHead(404, {
-    'Content-Type': 'text/html'
- })
-fs.createReadStream('../public/404.html').pipe(res);
+function send404(res) {
+    res.writeHead(404, {'Content-Type': 'text/html'});
+    fs.createReadStream('../public/404.html').pipe(res);
 }
 
-const mimeLookUp = {
+const mimeTable = {
     '.js': 'application/javascript',
     '.html': 'text/html',
     '.css': 'text/css'
 }
 
 const server = http.createServer((req, res) => {
-    let count = 0;
-   if(req.method == 'GET') {
+ if(req.method == 'GET') {
     let fileUrl = '';
+    
     if(req.url == '/' || req.url == '/home') fileUrl = '/index.html';
     else fileUrl = req.url;
-    let filePath = path.resolve('../public'+fileUrl);
+ 
+    console.log(`requested url : ${fileUrl}`);
+    const filePath = path.resolve('../public'+ fileUrl);
+    console.log(`filePath requested: ${filePath}`);
+    console.log('-------------------');
 
-    let fileExt = path.extname(filePath);
-    let mimeType = mimeLookUp[fileExt];
-    console.log(`${count+1} => ${req.url} `);
-    console.log(`fileExt: ${fileExt}`);
-    console.log(`mimeType: ${mimeType}`);
-    console.log(`fileUrl: ${fileUrl}`);
+    const fileExt = path.extname(filePath);
+    const mimeType = mimeTable[fileExt];
 
-
-    if(!mimeType) {
+    if(!mimeType){
         send404(res);
         return;
     }
 
-    fs.exists(filePath, (exists) => {
-        if(!exists){
+    fs.exists(filePath, (file) => {
+        if(!file){
             send404(res);
             return;
         }
-        res.writeHead(200, {'Content-Type': mimeType});
-        console.log(`filePath : ${filePath}`);
+        
         fs.createReadStream(filePath).pipe(res);
     })
 
-   }else{
-    send404(res)
-   }
-    
+ }else{
+    send404(res);
+    return;
+ }
 }).listen(3000);
-
-console.log('Server is running on port 3000');
+console.log('Broda server is running on port 3000.');
