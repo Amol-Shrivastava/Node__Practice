60833206f01b3f8c5a09c40e6ca6edd64a26c88c
amol <amolshrivastava448@gmail.com>
2022-11-03 23:07:27 +0530

    Basic authorisation

--- server/server.js
+++ server/server.js
@@ -19,11 +19,74 @@ const func1 = (msg) => {
 const gmFunc = func1("Good Morning :) ");
 const gnFunc = func1("Good Night :| ");
 
+const parseJSON = (req, res, next) => {
+    if(req.headers['content-type']=='application/json'){
+
+        let readData = '';
+        req.on('readable', () => {
+            readData += req.read();
+        })
+
+        req.on('end',()=> {
+            try{
+                req.body = JSON.parse(readData);
+            }catch(error){
+                // res.end('error');
+            }
+
+            next();
+        })
+
+    }else{
+        next();
+    }
+}
+
+const send401 = (res) => {
+    res.writeHead(401, {'WWW-Authenticate': 'Basic'});
+    res.end();
+}
+
+const auth = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    console.log(`authHeader ==> ${authHeader}`);
+
+    if(!authHeader){
+        send401(res);
+        return;
+    }
+
+    const val = authHeader.split(' ')[1];
+    console.log(`val ==> ${val}`);
+    const auth = Buffer.from(val, 'base64').toString().split(':');
+    const user = auth[0];
+    const password = auth[1];
+    console.log(`username ${user} password ${password}`);
+    if(user == 'foo' && password == 'bar'){
+        next();
+    }else{
+        send401(res);
+    }
+}
+
 
 connect()
 .use(logit)
 .use('/gm', gmFunc)
 .use('/gn', gnFunc)
+.use(parseJSON)
+.use(auth)
+.use((req, res) => {
+    res.end('Authorized :)');
+})
+.use((req,res) => {
+    if(req.body){
+        res.end('JSON parsed!, value of foo: '+req.body.foo);
+    }else{
+        res.end('no JSON detected');
+    }
+    // console.log(req.body);
+})
 .use((req, res, next) => res.write('Wassup'))
 .listen(3000);
 console.log('Server is running on port 3000 middleware');
