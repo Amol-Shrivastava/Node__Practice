ba7709f9b7cb8e9d3b971fea97af2b2a2c144ab2
amol <amolshrivastava448@gmail.com>
2022-11-09 00:48:30 +0530

    Cookie Basics over

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const sessionCookie = require('cookie-session');
+const cookieSession = require('cookie-session');
 const app = express();
 
 //1. Basic cookie example
@@ -56,15 +58,39 @@ const app = express();
 // }).listen(3000)
 
 //6. Secure cookie sent over HTTPS only
-app.use(cookieParser())
-.use((req, res) => {
-    if(req.cookies.name) {
-        res.clearCookie('name');
-        res.send('Value of secure cookie deleted: '+req.cookies.name)
-    }else{
-        res.cookie('name', 'resteer', {secure: true});
-        res.send('Now cookie can only be shared over https')
+// app.use(cookieParser())
+// .use((req, res) => {
+//     if(req.cookies.name) {
+//         res.clearCookie('name');
+//         res.send('Value of secure cookie deleted: '+req.cookies.name)
+//     }else{
+//         res.cookie('name', 'resteer', {secure: true});
+//         res.send('Now cookie can only be shared over https')
+//     }
+// }).listen(3000);
+
+//7. Session Cookie --> Used to store user specific information
+app.use(sessionCookie({
+    keys: ['This is my first session cookie']
+}))
+.use(cookieSession({
+    name: 'Session__Cookie_first',
+    keys: 'This is my custom session object key'
+}))
+.use('/home',(req, res) => {
+    if(req.session.views){
+        req.session.views++;
+        req.cookieS('name', 'sessionCookie')
     }
+    else 
+        req.session.views = 1;
+    
+    res.end(`Total views for you: ${req.session.views}`)
+})
+.use('/reset', (req, res) => {
+    delete req.session.views;
+    res.end('Cleared the view session')
 }).listen(3000);
+
 
 console.log('Broda express server is running on port 3000');
